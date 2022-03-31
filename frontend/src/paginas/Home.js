447390d2707a03288b13222eae8dd0f93224e97a
kevin-keise <kevinkeise@gmail.com>
2022-03-31 08:29:05 -0300

    criado arquivo header.js

--- frontend/src/paginas/Home.js
+++ frontend/src/paginas/Home.js
@@ -1,9 +1,10 @@
+import Header from '../componentes/Header';
 import './Home.css';
 
 function Home() {
     return (
       <div className="home-container">
-        <h1>Home</h1>
+        <Header/>
       </div>
     );
   }
