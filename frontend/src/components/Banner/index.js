8b7a39cbfd8169a7ac98f4101342d08a223b0bac
Guilherme Santos <santosgui678@gmail.com>
2022-04-03 19:11:32 -0300

    Refatorando código

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -1,9 +1,11 @@
 import './style.css';
+
 import img1 from '../../images/img-esquerda-banner.jpg';
 import img2 from '../../images/img-centro-banner.jpg';
 import img3 from '../../images/img-direita-banner.jpg';
-function Banner(){
-    return(
+
+function Banner() {
+    return (
         <div className='banner-container'>
             <div className='banner-container-centro'>
                 <div className='banner-foto-esquerda'>
@@ -12,12 +14,14 @@ function Banner(){
                         <span>BLUSA R$ 30,00</span>
                     </div>
                 </div>
+
                 <div className='banner-foto-centro'>
                     <img src={img2} />
                     <div>
                         <span>BLUSA R$ 30,00</span>
                     </div>
                 </div>
+
                 <div className='banner-foto-direita'>
                     <img src={img3} />
                     <div>
---
+++ frontend/src/components/ProductBox/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+function BoxProduct({ img, title, preco }) {
+    return (
+        <div className="box-product-container">
+            <a href='#'>
+                <div className='box-image-container'>
+                    <img src={img} />
+                </div>
+
+                <div>
+                    <h1>{title}</h1>
+                    <span>R$ {preco}</span>
+                </div>
+            </a>
+        </div>
+    );
+}
+
+export default BoxProduct;
