bfed5d6c4a5188812be8a5875211c3527d160a8c
Guilherme Santos <santosgui678@gmail.com>
2022-04-03 10:02:24 -0300

    Correção na Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Home from './pages/Home';
-import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Home />
     </div>
   );
 }
