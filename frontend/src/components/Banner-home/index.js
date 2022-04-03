22c951a258345207fb55e1da4b4ef7861d8e069f
Kevin Keise <kevinkeise@gmail.com>
2022-04-03 13:28:41 -0300

    banner layout finalizado (#22)

--- frontend/src/components/Banner-home/index.js
+++ frontend/src/components/Banner-home/index.js
@@ -8,12 +8,21 @@ function Banner(){
             <div className='banner-container-centro'>
                 <div className='banner-foto-esquerda'>
                     <img src={img1} />
+                    <div>
+                        <span>BLUSA R$ 30,00</span>
+                    </div>
                 </div>
                 <div className='banner-foto-centro'>
                     <img src={img2} />
+                    <div>
+                        <span>BLUSA R$ 30,00</span>
+                    </div>
                 </div>
                 <div className='banner-foto-direita'>
                     <img src={img3} />
+                    <div>
+                        <span>BLUSA R$ 30,00</span>
+                    </div>
                 </div>
             </div>
         </div>
