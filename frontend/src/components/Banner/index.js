68d367e4a9aed5db52f3a64cb1cd84377437b555
Guilherme Santos <santosgui678@gmail.com>
2022-04-30 10:10:32 -0300

    Imagens clicáveis no banner

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -1,3 +1,5 @@
+import BannerImage from '../BannerImage';
+
 import './style.css';
 
 import ProductsList from '../../products.js';
@@ -7,24 +9,30 @@ function Banner() {
         <div className='banner-container'>
             <div className='banner-container-centro'>
                 <div className='banner-foto-esquerda'>
-                    <img src={ProductsList[0].img} alt={ProductsList[0].description} loading="lazy" />
-                    <div>
-                        <span>{ProductsList[0].name}</span>
-                    </div>
+                    <BannerImage
+                        id={ProductsList[0].id}
+                        img={ProductsList[0].img}
+                        description={ProductsList[0].description}
+                        name={ProductsList[0].name}
+                    />
                 </div>
 
                 <div className='banner-foto-centro'>
-                    <img src={ProductsList[1].img} alt={ProductsList[2].description} loading="lazy" />
-                    <div>
-                        <span>{ProductsList[1].name}</span>
-                    </div>
+                    <BannerImage
+                        id={ProductsList[1].id}
+                        img={ProductsList[1].img}
+                        description={ProductsList[1].description}
+                        name={ProductsList[1].name}
+                    />
                 </div>
 
                 <div className='banner-foto-direita'>
-                    <img src={ProductsList[2].img} alt={ProductsList[2].description} loading="lazy" />
-                    <div>
-                        <span>{ProductsList[2].name}</span>
-                    </div>
+                    <BannerImage
+                        id={ProductsList[2].id}
+                        img={ProductsList[2].img}
+                        description={ProductsList[2].description}
+                        name={ProductsList[2].name}
+                    />
                 </div>
             </div>
         </div>
